24fb30cfe05375835f20d9a8702ea074d8b152ef
C-Mauro <maurocamila744@gmail.com>
2022-06-07 09:33:10 -0700

    comments

---
+++ hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+/**
+ * main-starting and ends program
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char str[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s", str);
+
+		return (0);
+}
