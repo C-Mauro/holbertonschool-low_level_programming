980c65ee6aefe5d385736183f31e34fef9576c80
C-Mauro <maurocamila744@gmail.com>
2022-06-10 10:56:56 -0700

    task 5

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *print_sign -print sings
+ *
+ *Return: return 1 0 and -1 depended of n value.
+ *
+ * @n: variable
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+		_putchar('+');
+	return (1);
+
+		if else(n == 0)
+		_putchar('0')
+			return (0);
+
+		if (n < 0)
+		_putchar('-')
+			return (-1);
+}
