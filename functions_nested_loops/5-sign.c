5849140adf320a8aab1a0fdb7c6c28d4c5500c9d
C-Mauro <maurocamila744@gmail.com>
2022-06-10 11:23:17 -0700

    task0

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -8,15 +8,23 @@
  */
 int print_sign(int n)
 {
+
 	if (n > 0)
+	{
 		_putchar('+');
 	return (1);
+	}
 
-		if else(n == 0)
-		_putchar('0')
+		else if (n == 0)
+		{
+		_putchar('0');
 			return (0);
+		}
 
-		if (n < 0)
-		_putchar('-')
+	else
+		{
+		_putchar('-');
 			return (-1);
+		}
+
 }
