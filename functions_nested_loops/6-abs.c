0e25bcc44299ea290eacc0a65f4e3af4e1b3ebaf
C-Mauro <maurocamila744@gmail.com>
2022-06-11 12:08:34 -0700

    abs

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_abs -print the absolute value of an integrer
+ *
+ *@x: integrer variable
+ *
+ * Return: 0 (succes)
+ */
+int _abs(int x)
+{
+
+	x = _abs(x);
+	return (0);
+
+}
