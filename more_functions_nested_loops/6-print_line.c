bd7ad175d52ddd54241210f7671f0fbde0bed61e
C-Mauro <maurocamila744@gmail.com>
2022-06-12 15:56:37 -0700

    comments and n<=0

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,20 +1,23 @@
 #include "main.h"
 /**
+ *print_line -print a line.
  *
+ * @n: number of times _ should be printed.
  *
  */
 void print_line(int n)
-{	
+{
 	int i;
-if ( n < 0)
+
+	if (n <= 0)
 {
 	_putchar('\n');
 }
 	else
 	{
-for (i=0 ; i<=n ; i++)
+	for (i = 1 ; i <= n ; i++)
 
-_putchar('_');
+	_putchar('_');
 	}
 	_putchar('\n');
 }
