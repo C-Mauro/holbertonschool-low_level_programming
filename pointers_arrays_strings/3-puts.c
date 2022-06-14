4c2a4770f173f8ef566786d272dce0750c0af33d
C-Mauro <maurocamila744@gmail.com>
2022-06-14 14:50:24 -0700

    print a string

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_puts -print the string
+ *
+ *@str: string.
+ */
+void _puts(char *str)
+{
+	 int i;
+
+	for (i = 0 ; str[i]!= '\0' ; )
+	{
+			_putchar(*str++);
+			
+	}
+	_putchar('\n');
+}
