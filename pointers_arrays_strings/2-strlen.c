584abdef9d0f433434f46f0e373262a2cec9f317
C-Mauro <maurocamila744@gmail.com>
2022-06-14 13:09:56 -0700

    betty style

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,15 +1,17 @@
-#include "main.h" 
+#include "main.h"
 /**
  *_strlen -return the length of a string.
  *
  *@s: string variable
+ *
+ * Return: return i , the length of a string.
  */
 int _strlen(char *s)
 {
 int i;
-	for(i=0 ; s [i] != '\0' ; i++)
+	for (i = 0 ; s[i] != '\0' ; i++)
 	{
 	}
 		return (i);
-	
+
 }
