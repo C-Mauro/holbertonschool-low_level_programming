4813ab65999a41e0fd3acf054e6dd256d8d0a608
C-Mauro <maurocamila744@gmail.com>
2022-06-15 14:50:28 -0700

    add a for

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -9,10 +9,14 @@ void puts2(char *str)
 	{
 		int i;
 
-		for (i = 0 ; str[i] != '\0' ; i = i + 1)
+		for (i = 0 ; str[i] != '\0' ; i++)
+		;
+		for (i=0 ; i < *str ; str = str +2)	
 		{
 		_putchar(*str);
-		str = str + 2;
+		
 		}
 		_putchar('\n');
+		
 	}
+
