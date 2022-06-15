7f01466e59b13d8cb959446a6e23dda179ca3f57
C-Mauro <maurocamila744@gmail.com>
2022-06-15 10:53:01 -0700

    print rev

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -7,11 +7,15 @@
 void print_rev(char *s)
 {
 	int i;
+
 	for (i = 0 ; s[i] != '\0' ; i++)
 	{
-	 for (i = 0 ;( i = 0) ; i--)
-		
-		s--;
-
+	
 	}
+	for ( ; i >= 0 ; i--)
+	{
+	_putchar(s[i]);
+	}
+	_putchar ('\n');
 }
+
