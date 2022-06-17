d619046ff6b8e9f446958d7f517d88f3e12d1553
C-Mauro <maurocamila744@gmail.com>
2022-06-17 09:45:44 -0700

    betty style

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,20 +1,22 @@
-#include "main.h" 
+#include "main.h"
 #include <stdio.h>
 /**
+ *print_array - print array separated by a comma and space.
  *
+ *@a: string address
  *
- *
+ *@n: string lenght
  */
 void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0 ; i < n ; i++) 
+	for (i = 0 ; i < n ; i++)
 	{
-		if (i == n - 1) 
+		if (i == n - 1)
 			printf("%d", a[i]);
-		else 
-			printf("%d, ",a[i]);
+		else
+			printf("%d, ", a[i]);
 	}
 	printf("\n");
 }
