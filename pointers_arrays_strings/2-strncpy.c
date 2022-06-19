5e10d45801ba1b401ba10e0f24f9d61c929021b8
C-Mauro <maurocamila744@gmail.com>
2022-06-19 11:34:30 -0700

    betty style

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -2,23 +2,23 @@
 /**
  *_strncpy -make a copy of a string.
  *
- *@src: first string 
+ *@src: first string
  *
  *@dest: second string
  *
- *@n: number of bytes 
+ *@n: number of bytes
  *
- * Return: dest 
+ * Return: dest
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i; 
+	int i;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
 
-	for (; i < n ; i++) 	
+	for (; i < n ; i++)
 	dest[i] = '\0';
-	
+
 	return (dest);
 }
