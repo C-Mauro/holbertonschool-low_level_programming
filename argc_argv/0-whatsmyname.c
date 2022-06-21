dffc5f9a5565b841c10ac3741d7d18a24a30a32f
C-Mauro <maurocamila744@gmail.com>
2022-06-21 15:37:01 -0700

    whats my name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <std.io>
+/**
+ *main -start the program
+ *@argc: arguments count
+ *@argv: pointer
+ *Return: zero
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s", argv[0])
+
+		return (0);
+}
