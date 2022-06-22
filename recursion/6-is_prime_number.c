9fede9df3deec4eca21744b0ed9eeeaa13a01687
C-Mauro <maurocamila744@gmail.com>
2022-06-21 18:43:26 -0700

    bracets

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,28 +1,38 @@
 #include "main.h"
+
 /**
+ * _recursive_division - returns 1 if number is prime
+ * @x: value
+ * @y: value
  *
- *
+ * Return: 0 or 1
  */
 
-int prime_num(int n, int m)
+int _recursive_division(int x, int y)
 {
-	if ((m != n) && (n != 1) && (n % m != 0))
+	if ((x % y != 0) && (x != y) && (x != 1))
 	{
-		prime_num(n, m + 1);
-	}		
-	else if ((n == 1) || (n < 0))
+		return (_recursive_division(x, (y + 1)));
+	}
+	else if ((x == 1) || (x < 0))
 	{
 		return (0);
 	}
 	else
-	{	
+	{
 		return (1);
 	}
-
-	return (1);
+	return (0);
 }
-	
+
+/**
+ * is_prime_number - same as above
+ * @n: number to analyze
+ *
+ * Return: 0 or 1
+ */
+
 int is_prime_number(int n)
 {
-	return(prime_num(n, 2));
+	return (_recursive_division(n, 2));
 }
