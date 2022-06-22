78384243bd998fc184db107dc2af52285ebafd7b
C-Mauro <maurocamila744@gmail.com>
2022-06-21 18:38:50 -0700

    bracets

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -5,18 +5,22 @@
  */
 
 int prime_num(int n, int m)
-	{
+{
 	if ((m != n) && (n != 1) && (n % m != 0))
+	{
 		prime_num(n, m + 1);
-			
+	}		
 	else if ((n == 1) || (n < 0))
+	{
 		return (0);
-
-	else 
-		return (1);
-
-	return (1);
 	}
+	else
+	{	
+		return (1);
+	}
+
+	return (0);
+}
 	
 int is_prime_number(int n)
 {
