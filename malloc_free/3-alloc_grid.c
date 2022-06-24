658faec115f8b5cb1ed861734a00951de50972a7
C-Mauro <maurocamila744@gmail.com>
2022-06-24 08:16:23 -0700

    grid

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -9,12 +9,14 @@ int **alloc_grid(int width, int height)
 	int **ptr;
 	int i,j;
 	if (width <= 0 || height <= 0)
+	{
 		return (NULL);
-
+	}
 	ptr = malloc(sizeof(int*) * height);
 	if (ptr == NULL)
+	{
 		return (NULL);
-
+	}
 	for (i = 0; i < height; i++)
 	ptr[i]=malloc(sizeof(int) * width);
 	if (ptr[i] == NULL)
