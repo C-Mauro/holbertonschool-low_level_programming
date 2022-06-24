e5270d14e449467bdb02c2916c8b0f09b505929a
C-Mauro <maurocamila744@gmail.com>
2022-06-24 08:29:15 -0700

    grid bracets

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -2,35 +2,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
-*
+*alloc_grid -grid
+*@width: whidth to the grid
+*@height: height to the grid
+*Return: ptr
 */
 int **alloc_grid(int width, int height)
 {
 	int **ptr;
-	int i,j;
+	int i, j;
+
 	if (width <= 0 || height <= 0)
 	{
 		return (NULL);
 	}
-	ptr = malloc(sizeof(int*) * height);
+	ptr = malloc(sizeof(int *) * height);
 	if (ptr == NULL)
 	{
 		return (NULL);
 	}
 	for (i = 0; i < height; i++)
-	ptr[i]=malloc(sizeof(int) * width);
+	{
+	ptr[i] = malloc(sizeof(int) * width);
+
 	if (ptr[i] == NULL)
 	{
 		for (; i >= 0; i--)
 		{
-		free (ptr[i]);
+		free(ptr[i]);
 		}
-		free (ptr);
+		free(ptr);
 		return (NULL);
 	}
 	for (j = 0; ptr[i][j]; j++)
 	{
 		ptr[i][j] = 0;
+	}
 	}
 	return (ptr);
 }
