d907436ad9da8b9629a38265cd9734dfe5c5e24f
C-Mauro <maurocamila744@gmail.com>
2022-06-26 17:13:35 -0700

    j < n

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -25,9 +25,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s2 = "";
 	}
 	if (n >= size2)
-		n=size2;
+		n = size2;
 
-	s3 = malloc(size1 + size2 + 1 * sizeof(char));
+	s3 = malloc(size1 + n + 1 * sizeof(char));
 
 	if (s3 == NULL)
 	return (NULL);
