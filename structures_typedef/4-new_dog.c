aba355686bfd153eb9284fc3cfdb2392f2abe0a1
C-Mauro <maurocamila744@gmail.com>
2022-06-30 09:35:17 -0700

    new dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,41 +1,48 @@
 #include "dog.h"
 #include "stdlib.h"
 /**
- *
+ *_strlen -string lenght
+ *@s: string
+ *Return: number of bytes (string)
  */
 int _strlen(char *s)
 {
 	int i;
+
 	for (i = 0 ; s[i] != '\0' ; i++)
 		;
 		return (i);
 }
+/**
+ *new_dog -create a new dog type
+ *@name: dog's name
+ *@age: dog's age
+ *@owner: dog's owner
+ *Return: pointer
+ */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	int i;
-	int Lname, Lowner;
+	int i, Lname, Lowner;
 	dog_t *ptr;
 
 	ptr = malloc(sizeof(dog_t));
 
-	if(ptr == NULL)
+	if (ptr == NULL)
 		return (NULL);
-	
+
 	Lname = _strlen(name);
 	Lowner = _strlen(owner);
 
-	ptr->name = malloc (Lname + 1);
-
-	if (ptr->name)
+	ptr->name = malloc(Lname + 1);
+	if (ptr->name == NULL)
 	{
 		free(ptr->name);
 		free(ptr);
-		return(NULL);
+		return (NULL);
 	}
 	ptr->owner = malloc(Lowner + 1);
-
-	if(ptr->owner)
+	if (ptr->owner == NULL)
 	{
 		free(ptr->owner);
 		free(ptr->name);
@@ -44,15 +51,13 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 	ptr->age = age;
 
-	for(i = 0; i < Lname; i++)
-	{
+	for (i = 0; i < Lname; i++)
 		ptr->name[i] = name[i];
-		ptr->name[i] = '\0';
-	}
-	for(i = 0; i < Lowner; i++)
-	{
+	ptr->name[i] = '\0';
+
+	for (i = 0; i < Lowner; i++)
 		ptr->owner[i] = owner[i];
-		ptr->owner[i] = '\0';
-	}
+	ptr->owner[i] = '\0';
+
 	return (ptr);
 }
