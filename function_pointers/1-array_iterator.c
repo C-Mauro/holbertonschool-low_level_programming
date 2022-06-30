ed2faa71f80872900f11be240b6dc2ff9535aab9
C-Mauro <maurocamila744@gmail.com>
2022-06-30 13:16:48 -0700

    array iterator

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ *array_iterator -function execute a function a[]
+ *@array: array
+ *@size: size of the array
+ *@action: pointer to a function
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+	{
+	unsigned int i;
+
+	if (action == NULL && array == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+
+	}
