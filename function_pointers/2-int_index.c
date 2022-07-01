27601c440999b5f59b289e6a158046715b2ec09d
C-Mauro <maurocamila744@gmail.com>
2022-06-30 18:39:28 -0700

    for and return

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -2,19 +2,22 @@
 #include <stdio.h>
 /**
  *int_index -seach for an integrer
- *
- *
+ *@array: array
+ *@size: size of array
+ *@cmp: pointer to function
+ *Return: i matches, -1 not match
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i; 
-	if (array == NULL || cmp == NULL ||size <= 0)
-		return (-1); 
+	int i;
 
-	for(i = 0; i < size; i++)
-	{	
+	if (array == NULL || cmp == NULL || size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
 		if (cmp(array[i]))
 			return (i);
 	}
-	return(-1);
+	return (-1);
 }
