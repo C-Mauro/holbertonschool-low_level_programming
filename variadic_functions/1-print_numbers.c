baf30b947c5f88d115b6568842e550cc18399849
C-Mauro <maurocamila744@gmail.com>
2022-07-04 09:19:22 -0700

    change num = va_arg into the for

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -17,14 +17,16 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	if (separator == NULL)
 		separator = "";
 
-	num = va_arg(arg, int);
 
 	for (i = 0; i < n; i++)
 	{
 		if (i != 0)
 			printf("%d%s", num, separator);
+
 		else
 			printf("%d", num);
+
+		num = va_arg(arg, int);
 	}
 	printf("\n");
 
