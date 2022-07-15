01a1103378834a3286b26c1d2a8a1f7fcaafd3ac
C-Mauro <maurocamila744@gmail.com>
2022-07-15 15:04:32 -0700

    put the temp->next = newnode outside of the for

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -6,9 +6,10 @@
  *add_node_end -add a new node at the end of the list.
  *@head: pointer to a pointer with the first element address.
  *@str: te address of the new pointer.
+ *Return: address of the new node.
  */
 list_t *add_node_end(list_t **head, const char *str)
-{	
+{
 	int i;
 	list_t *temp;
 	list_t *newnode = malloc(sizeof(list_t));
@@ -16,17 +17,17 @@ list_t *add_node_end(list_t **head, const char *str)
 	if (newnode == NULL)
 		return (NULL);
 
- 	newnode->str = strdup(str);
+	newnode->str = strdup(str);
 
-	if(newnode->str == NULL)
+	if (newnode->str == NULL)
 	{
 		free(newnode);
 		return (NULL);
 	}
-	
+
 	newnode->len = _strlen(newnode->str);
 	newnode->next = NULL;
-	
+
 	temp = *head;
 
 	if (*head == NULL)
@@ -34,12 +35,14 @@ list_t *add_node_end(list_t **head, const char *str)
 		*head = newnode;
 		return (newnode);
 	}
-	for(i = 0; temp->next != NULL; i++)
+	for (i = 0; temp->next != NULL; i++)
 	{
 		temp = temp->next;
-		temp->next = newnode;
 	}
+		temp->next = newnode;
+
 	return (newnode);
+	free(newnode);
 
 }
 /**
@@ -50,6 +53,7 @@ list_t *add_node_end(list_t **head, const char *str)
 int _strlen(char *s)
 {
 	int i;
+
 	for (i = 0; s[i] != '\0'; i++)
 		;
 	return (i);
