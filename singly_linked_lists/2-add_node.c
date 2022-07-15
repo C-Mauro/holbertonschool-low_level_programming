26f2475f29bfb302f5113c5814adb8635c2bf9e1
C-Mauro <maurocamila744@gmail.com>
2022-07-15 06:37:25 -0700

    betty style

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -3,8 +3,10 @@
 #include <stdio.h>
 #include <string.h>
 /**
- *
- *
+ *add_node -this function add a new node at the begining of the list.
+ *@head: pointer to a pointer with the first element address.
+ *@str: pointer to a string
+ *Return: the address of the new node
  */
 list_t *add_node(list_t **head, const char *str)
 {
@@ -13,26 +15,31 @@ list_t *add_node(list_t **head, const char *str)
 	if (newnode == NULL)
 		return (NULL);
 
- 	newnode->str = strdup(str);
+	newnode->str = strdup(str);
 
-	if(newnode->str == NULL)
+	if (newnode->str == NULL)
 	{
 		free(newnode);
 		return (NULL);
 	}
-	
+
 	newnode->len = _strlen(newnode->str);
 
 	newnode->next = *head;
 	*head = newnode;
 
-	return(newnode);
+	return (newnode);
 
 }
-
+/**
+ *_strlen -lenght of the string (work like strlen)
+ *@s: string
+ *Return: string lenght.
+ */
 int _strlen(char *s)
 {
 	int i;
+
 	for (i = 0; s[i] != '\0'; i++)
 		;
 	return (i);
