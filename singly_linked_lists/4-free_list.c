1a2e854ad37446f8e555aa426903214ecdfbde85
C-Mauro <maurocamila744@gmail.com>
2022-07-15 17:14:19 -0700

    betty style

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -10,11 +10,11 @@ void free_list(list_t *head)
 {
 	list_t *temp;
 
-for (; head != NULL ; )
-    {
+	for (; head != NULL ; )
+	{
 	temp = head;
 	head = head->next;
 	free(temp->str);
 	free(temp);
-    }
+	}
 }
