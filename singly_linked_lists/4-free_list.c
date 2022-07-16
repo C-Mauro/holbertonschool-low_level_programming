e6a09fe924e860c3af2a4f9742e455e850d42796
C-Mauro <maurocamila744@gmail.com>
2022-07-15 17:11:20 -0700

    free list

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *free_list -free a list_t
+ *@head: pointer with the address to the first element of the list
+ *Return: 0
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+for (; head != NULL ; )
+    {
+	temp = head;
+	head = head->next;
+	free(temp->str);
+	free(temp);
+    }
+}
