246999b6c5cb3d44a51b997261fbd03f46e3c57a
C-Mauro <maurocamila744@gmail.com>
2022-07-15 18:22:01 -0700

    free list 2

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *free_listsint2 -free list but set null to the head
+ *@head: pointer to a pointer (address to the first element of the list)
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	for (; head != NULL ; )
+	{
+		temp = head;
+		head = (head->next);
+		free(temp);
+		head = NULL;
+	}
+}
+
