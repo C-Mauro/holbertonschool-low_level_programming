969af004c3ba5d99488d6ea26048a5acd6135be2
C-Mauro <maurocamila744@gmail.com>
2022-07-15 18:39:47 -0700

    5 free list2 add *

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -9,12 +9,15 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	for (; head != NULL ; )
+	if (*head == NULL)
+		return;
+
+	for (; *head != NULL ; )
 	{
-		temp = head;
-		head = (head->next);
+		temp = *head;
+		*head = (*head)->next;
 		free(temp);
-		head = NULL;
 	}
+	*head = NULL;
 }
 
