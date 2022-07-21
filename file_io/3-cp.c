bc14a055065153b0ac2308efbdb3c87d79f2e29a
C-Mauro <maurocamila744@gmail.com>
2022-07-20 19:55:32 -0700

    copy a string

---
+++ file_io/3-cp.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <string.h>
+/**
+ * @brief 
+ * 
+ */
+
+int main(int argc, char *argv[])
+{
+	int ofile_from, ofile_to, cfile, rfile, wfile;
+	char buffer[1024];
+
+	if (argc < 3)
+	{
+		fprintf(stderr, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	ofile_from = open(argv[1], O_RDONLY);
+	if (ofile_from == -1)
+	{
+		fprintf(stderr, "Error: Can't read from %s\n", argv[1]);
+		exit(98);
+	}
+	ofile_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (ofile_to == -1)
+	{
+		cfile = close(ofile_from);
+		if (cfile == -1)
+		{
+			fprintf(stderr, "Error: Can't close fd %d", ofile_from);
+			exit(100);
+		}
+		fprintf(stderr, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	rfile = read(ofile_from, buffer, 1024);
+	wfile = write(ofile_to, buffer, 1024);
+
+	return (0);
+}
