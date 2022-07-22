ed293bdc40c834b2204fa882f818de34523701e9
C-Mauro <maurocamila744@gmail.com>
2022-07-22 15:46:18 -0700

    sum

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ *sum_dlistint -sum of all the data n 
+ *@head: pointer to the first element of the linked list
+ *Return: result of the sum
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	for (; head != NULL;)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
