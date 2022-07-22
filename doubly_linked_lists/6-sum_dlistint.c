7630e4c6aabb5323c1e182d2d9e7ba2ac5ce51ab
C-Mauro <maurocamila744@gmail.com>
2022-07-22 15:47:16 -0700

    kernel style

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- *sum_dlistint -sum of all the data n 
+ *sum_dlistint -sum of all the data n
  *@head: pointer to the first element of the linked list
  *Return: result of the sum
  */
