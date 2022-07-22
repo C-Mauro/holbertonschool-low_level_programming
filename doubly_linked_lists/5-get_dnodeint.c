be9662268273bd3e8683ded4759267ca99311094
C-Mauro <maurocamila744@gmail.com>
2022-07-22 15:55:05 -0700

    nth node

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ *get_dnodeint_at_index -function return the node of nth node of the linked list
+ *@head: pointer to the first element of the list
+ *Return: nth.
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i;
+	dlistint_t *temp = head;
+
+	if (head == NULL)
+		return (NULL);
+
+	if (index == 0)
+		return (head);
+
+	for (i = 0; i < index; i++)
+	{
+		if (temp->next == NULL)
+			return (NULL);
+		temp = temp->next;
+	}
+	return (temp);
+}
