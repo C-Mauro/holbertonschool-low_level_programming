3a797bab5624c35e9fe3464e3510ea3e7dacd15d
C-Mauro <maurocamila744@gmail.com>
2023-01-03 13:25:21 -0800

    index

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -12,15 +12,21 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-	size_t i = 0;
+	size_t i;
 
 	if (!array)
 		return (-1);
 
-	for (; i < size; i++)
+
+	for (i = 0; i < size; i++)
 	{
+
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+
 		if (array[i] == value)
-			return (value);
+		{
+			return (array[i]);
+		}
 	}
 
 	return (-1);
