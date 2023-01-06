a0f43998e396f41bb0b24415d4af511a77d1acc9
C-Mauro <maurocamila744@gmail.com>
2023-01-06 09:43:43 -0800

    fix the size (size - 1) add the prinf and betty

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -1,26 +1,44 @@
 #include "search_algos.h"
 #include <stdio.h>
 /**
- *binary_search -
+ *binary_search -Compares key value to the middle element of the array
+ *@array: array
+ *@size: size of the entire array
+ *@value: value to search for.
  *
+ *Return: mid if is succesfull (value you search), otherwise -1
  */
 
 int binary_search(int *array, size_t size, int value)
 {
-	size_t left = 0;
+	size_t left = 0, i;
 	size_t mid = 0;
-	
-	if (!array)
-		return(-1);
 
-	while (size >= left)	
+	if (!array)
+		return (-1);
+
+
+	while (left <= size - 1)
 	{
 
-		mid = (size + left) / 2;
+		mid = ((size - 1) + left) / 2;
+
+		printf("Searching in array: ");
+
+		for (i = left; i <= size - 1; i++)
+		{
+			if (i != left)
+				printf(", %d", array[i]);
+			else
+				printf("%d", array[i]);
+		}
+
+		printf("\n");
+
 
 		if (array[mid] == value)
 		{
-			return(mid);
+			return (mid);
 		}
 		else if (array[mid] > value)
 		{
@@ -30,7 +48,7 @@ int binary_search(int *array, size_t size, int value)
 		else
 			left = mid + 1;
 
-	}	
-	return(-1);
 
+	}
+	return (-1);
 }
